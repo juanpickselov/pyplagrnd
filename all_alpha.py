2ba3a201d41f2bf38ee84c2848c1345f1199a877
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-10-12 17:19:58 -0400

    set stuff

--- all_alpha.py
+++ all_alpha.py
@@ -1,5 +1,4 @@
-alpha_set = {'a', 'b', 'c'}
-alfaset = set('abcdefghijklmnopqrstuvwxyz')
+alpha_set = set('abcdefghijklmnopqrstuvwxyz')
 sentence_set = {'x', 'a', 'c', 'b'}
 
 
@@ -11,7 +10,8 @@ print(includes_alphabet())
 sentence_set = set('a quick red fox jumps over the lazy brown dog')
 print(includes_alphabet())
 
-sentence_set = set('the big wild zoo just recently having quieted down for the known season hush keeping extra busy customers away')
+sentence_set = set('the big wild zoo just recently having quieted down '
+                   'for the known season hush keeping extra busy customers away')
 print(includes_alphabet())
 
 sentence_set = set("let's not go yet")
