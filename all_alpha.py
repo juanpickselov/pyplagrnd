49a94fb6e7b6ed8b3d544dab834baa29cb85ee33
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-10-12 17:08:17 -0400

    set stuff

---
+++ all_alpha.py
@@ -0,0 +1,18 @@
+alpha_set = {'a', 'b', 'c'}
+alfaset = set('abcdefghijklmnopqrstuvwxyz')
+sentence_set = {'x', 'a', 'c', 'b'}
+
+
+def includes_alphabet():
+    return alpha_set.issubset(sentence_set)
+
+print(includes_alphabet())
+
+sentence_set = set('a quick red fox jumps over the lazy brown dog')
+print(includes_alphabet())
+
+sentence_set = set('the big wild zoo just recently having quieted down for the known season hush keeping extra busy customers away')
+print(includes_alphabet())
+
+sentence_set = set("let's not go yet")
+print(includes_alphabet())
