f45be927c7bc4d8e63248350ad2d25aedb81103c
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-10-15 17:31:48 -0400

    chapter folders wip

---
+++ file_management/test_phylz.py
@@ -0,0 +1,3 @@
+from file_management.file_utils import make_sub_folders
+
+make_sub_folders('/zPy/eff_py', 9)
--- test_phylz.py
+++
@@ -1,3 +0,0 @@
-from phylz import make_sub_folders
-
-make_sub_folders('/zPy/eff_py', 9)
