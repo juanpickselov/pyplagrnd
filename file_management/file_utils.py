6b0ca27b712015171c194a5d62cfc129d4dba535
juanpickselov <juanpickselov@sbcglobal.net>
2018-12-01 21:53:46 -0500

    pep 8 stuff

--- file_management/file_utils.py
+++ file_management/file_utils.py
@@ -2,11 +2,13 @@ import os
 import pathlib
 
 
-def what_is_the_working_dir():
+def working_dir():
     return os.getcwd()
+
 
 def make_a_folder(folder_to_make):
     pathlib.Path(folder_to_make).mkdir(parents=True, exist_ok=True)
+
 
 def make_sub_folders(parent_folder, num_sub_folders):
     for x in range(1, num_sub_folders):
